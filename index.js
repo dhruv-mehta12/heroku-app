53585ca76649c1d80e4a0d4579a460c06351e579
dhruvMehta1208 <mehtadhruv2k1@gmail.com>
2022-07-17 14:55:46 +0530

    update

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
-    console.log(`application running on port ${PORT}`)
+    console.log(`application running on port ${port}`)
 })
